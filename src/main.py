790581ef44b3174da0c2a649c97fcd163537816d
zuhairimsafocus-sketch <zuhairimsafocus@gmail.com>
2026-05-21 16:07:32 +0800

    Fix limiter and metrics

--- src/main.py
+++ src/main.py
@@ -36,18 +36,12 @@ class CustomerInput(BaseModel):
 
 app = FastAPI()
 
-instrumentator = Instrumentator(
-    should_group_status_codes=False
-)
-
+instrumentator = Instrumentator()
 instrumentator.instrument(app)
+instrumentator.expose(app)
 
-@app.on_event("startup")
-async def startup():
-    instrumentator.expose(
-        app,
-        endpoint="/metrics"
-    )
+limiter = Limiter(key_func=get_remote_address)
+app.state.limiter = limiter
 
 API_KEY = os.getenv("API_KEY")
 
