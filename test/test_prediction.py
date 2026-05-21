2d151e5aea2d3d14bdd34dc4cced40c22805e0be
zuhairimsafocus-sketch <zuhairimsafocus@gmail.com>
2026-05-21 14:48:16 +0800

    Fix API key for tests

--- test/test_prediction.py
+++ test/test_prediction.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from src.main import app
+import os
 
 client = TestClient(app)
 
@@ -25,9 +26,10 @@ def test_prediction():
 
     response = client.post(
         "/predict",
-        headers={
-            "api-key":"zul12345"
+       headers={
+    "api-key": os.getenv("API_KEY", "zul12345")
         },
+        
         json=sample
     )
 
